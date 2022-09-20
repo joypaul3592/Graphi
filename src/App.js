1cb6b8edf8daecb94a814f4f7c0c3b261ed7a97a
Mosiur <mosiurislamwebdesign@gmail.com>
2022-09-21 02:17:39 +0600

    add

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import ReactGraph from './Components/ReactGraph/ReactGraph';
 function App() {
 
   const rawData = [
-    { label: 'Mon', aValue: 40, bValue: 62 },
+    { label: 'Mon', aValue: 200, bValue: 62 },
     { label: 'Tue', aValue: 14, bValue: 68 },
     { label: 'Wed', aValue: 22, bValue: 76 },
     { label: 'Thu', aValue: 43, bValue: 54 },
-    { label: 'Fri', aValue: 33, bValue: 58 },
+    { label: 'Fri', aValue: 33, bValue: 58 }
   ];
+  // { label: 'Tue', aValue: 14, bValue: 68 },
+  // { label: 'Wed', aValue: 22, bValue: 76 },
+  // { label: 'Thu', aValue: 43, bValue: 54 },
+  // { label: 'Fri', aValue: 33, bValue: 58 },
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      <div className=' w-1/2 mx-auto mt-40'> <BarChart2 data={rawData} /></div>
-      <div className=' w-1/2 mx-auto my-40  p-2'><ReactGraph></ReactGraph></div>
+      <div className=' w-1/2 mx-auto mt-40'> <BarChart2 Data={rawData} /></div>
+      {/* <div className=' w-1/2 mx-auto my-40  p-2'><ReactGraph></ReactGraph></div> */}
     </div>
   );
 }
