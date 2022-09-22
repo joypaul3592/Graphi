98d27d74586740d503a00023415a87137fc6f668
Mosiur <mosiurislamwebdesign@gmail.com>
2022-09-23 00:51:18 +0600

    ds

--- src/Components/Chart/SimpleLineChart2/SimpleLineChart2.js
+++ src/Components/Chart/SimpleLineChart2/SimpleLineChart2.js
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from 'react'
 import { Bar, Line } from "react-chartjs-2";
 import 'chartjs-plugin-dragdata'
 
+export default function SimpleLineChart2() {
+    const [Data, setData] = useState([])
+    const [dataisLoaded, setdataisLoaded] = useState(false)
 
-export default function SimpleLineChart2(props) {
+
+
     const [shouldRedraw] = useState(false);
     const [isLoaded, setIsLoaded] = useState(false);
 
 
-    const buildDataSet = () => {
-
+    const buildDataSet = (data) => {
+        let labels = data?.map(c => c.label);
         var options = {
             type: 'line',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: labels,
                 datasets: [{
                     label: '# Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: data?.map(c => c.yValue),
                     // borderColor: 'rgb(75, 192, 192)',
                     fill: true,
                     tension: 0.4,
@@ -60,7 +64,7 @@ export default function SimpleLineChart2(props) {
     }
 
 
-    let localOption = buildDataSet(props.data);
+    let localOption = buildDataSet(Data);
 
 
     useEffect(() => {
@@ -69,8 +73,89 @@ export default function SimpleLineChart2(props) {
         }, 200);
     }, [])
 
+
+    useEffect(() => {
+        fetch('https://intense-river-05869.herokuapp.com/api/v1/grap/dualLine', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data?.data)
+            })
+
+    }, [dataisLoaded])
+
+    /* ===================== Data Delete =========  */
+    const [Delete, setDelete] = useState()
+    if (Delete) {
+        const id = Delete;
+        fetch(`https://intense-river-05869.herokuapp.com/api/v1/grap/dualLine/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data) {
+                    setdataisLoaded(!dataisLoaded)
+                }
+            })
+    }
+
+
+
+    /* ===================== Data Post =========  */
+    const [label, setlabel] = useState('')
+    const [yValue, setaValue] = useState(0)
+    const [xValue, setxValue] = useState(0)
+    const submitPost = (label, yValue, xValue) => {
+        if (label && yValue && xValue) {
+            const Data = { label: label, yValue: yValue, xValue: xValue }
+            fetch('https://intense-river-05869.herokuapp.com/api/v1/grap/dualLine', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(Data),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data) {
+                        setdataisLoaded(!dataisLoaded)
+                    }
+                })
+
+        }
+    }
+
+
+
+
+
     return (
         <div>
+            <div>
+                <input onBlur={(e) => setlabel(e.target.value)} type="text" placeholder='Names' className='border-4' />
+                <input onBlur={(e) => setaValue(e.target.value)} type="number" placeholder='Number' className='border-4' />
+                <input onBlur={(e) => setxValue(e.target.value)} type="number" placeholder='Number' className='border-4' />
+                <button onClick={() => submitPost(label, yValue,xValue)} type="button" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Submit</button>
+            </div>
+            {/* ============ ============= */}
+            <div className='grid grid-cols-3'>
+                {
+                    Data.map(data => <div key={data._id}>
+                        <div className='flex gap-10'>
+                            <p>{data.label}</p>
+                            <p>{data.yValue}</p>
+                            <p onClick={() => setDelete(data._id)} className='text-red-500 cursor-pointer border-2 bg-black'>X</p>
+                        </div>
+                    </div>)
+                }
+            </div>
             {isLoaded &&
                 <Line
                     redraw={shouldRedraw}
