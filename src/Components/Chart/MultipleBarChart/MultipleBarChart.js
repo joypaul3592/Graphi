f0c8702d5d66fa58dc07e25ac659d4d9528a9b2e
Mosiur <mosiurislamwebdesign@gmail.com>
2022-09-27 09:47:24 +0600

    ass

--- src/Components/Chart/MultipleBarChart/MultipleBarChart.js
+++ src/Components/Chart/MultipleBarChart/MultipleBarChart.js
@@ -74,7 +74,6 @@ export default function MultipleBarChart() {
                         },
                         // Only change when finished dragging 
                         onDragEnd: function (e, datasetIndex, index, value) {
-                            console.log(e, datasetIndex, index, value)
                             fetch(`http://localhost:5000/api/v1/grap/multipleBar/${index}`, {
                                 method: 'PATCH',
                                 headers: {
@@ -171,7 +170,7 @@ export default function MultipleBarChart() {
                 })
 
         }
-        event.preventdefault()
+        event.preventDefault()
     }
     return (
         <div className=' h-full flex justify-center items-center'>
@@ -182,7 +181,7 @@ export default function MultipleBarChart() {
                         <div className='flex flex-col w-full my-10 md:my-0 md:w-[50%] p-5 rounded-md shadow-md xl:px-10'>
                             <h1 className=' text-purple-800 font-semibold '>Add Your Graph</h1>
                             <hr className='mb-4 mt-1 bg-purple-800 h-[1.5px] w-1/2 flex mx-auto' />
-                            <form onClick={() => SubmitPost()} className='flex flex-col'>
+                            <form onClick={SubmitPost} className='flex flex-col'>
                                 <input type="text" placeholder='Names' name='names' className='bg-gray-100 px-3 py-1 rounded shadow-md' />
 
                                 <input type="number" placeholder='Number' name='number' className='bg-gray-100 mt-4 px-3 py-1 rounded shadow-md' />
