ec908e4d39322dfddc80a3f42bb569e81e184d07
Joy Paul <joypaul3592@gmail.com>
2022-10-04 23:59:18 +0600

    update by job

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,120 +1,182 @@
 import React, { useEffect, useState } from 'react';
-import { useAuthState, useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import 'react-toastify/dist/ReactToastify.css';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuthState, useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { toast } from 'react-toastify';
-import './Login.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { MdOutlineCancel } from "react-icons/md";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import auth from '../../firebase.init';
-import Sociallogin from './Sociallogin';
+
 
 const Login = () => {
-  const navigate = useNavigate();
-  const location = useLocation()
 
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword, loguser, loading, error,] = useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending, resetError] = useSendPasswordResetEmail(auth);
+  const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
   const [user] = useAuthState(auth)
 
-  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(
-    auth
-  );
-  const [
-    signInWithEmailAndPassword,
-    user2,
-    loading2,
-    error2,
-  ] = useSignInWithEmailAndPassword(auth);
- 
-  const handleSignin = async (event) => {
-    event.preventDefault()
-    await signInWithEmailAndPassword(email, password)
+  const location = useLocation()
+  const naviget = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  // Error
+  const [emailError, setEmailError] = useState('');
+  const [passError, setPassError] = useState('');
+  const [state, setState] = useState(false)
+
+
+  // email input value & validation
+  const emailClick = (e) => {
+    const EmailRegex = /\S+@\S+\.\S+/;
+    const emailValue = EmailRegex.test(e.target.value);
+    if (emailValue) {
+      setEmail(e.target.value);
+      setEmailError('')
+    } else {
+      setEmailError('Please give the valid Email')
+      setEmail('');
+    }
   }
 
-  if (error2) {
-    toast(error2.message)
+
+  // Password input value 
+  const passwordClick = (e) => {
+    const passwordValue = e.target.value;
+    if (passwordValue.length >= 6) {
+      setPassword(passwordValue);
+      setPassError('')
+    } else {
+      setPassword('');
+      setPassError('Please Give Minimum 6 Chrecter')
+    }
   }
 
 
 
 
-  if (sending) {
-    toast("check your email")
+  const toggleBtn = () => {
+    setState(prevState => !prevState)
   }
-  let from = location.state?.from?.pathname || "/";
- 
-if (user) {
- 
- navigate(from, { replace: true })
-}
+
+
+  // handel Login
+
+  const handelLogin = (e) => {
+    if (email !== '' && password !== '') {
+      signInWithEmailAndPassword(email, password)
+      setEmailError('')
+      setPassError('')
+    } else {
+      setEmailError('Please Fill The Input')
+      setPassError('Please Give Me the Password')
+    }
+    e.preventDefault()
+  }
+
+
+  // requirAuth
+  const from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (user) {
+      const url = `https://secure-depths-99773.herokuapp.com/login`;
+      fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: user.email
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          localStorage.setItem('accessToken', data.token)
+          naviget(from, { replace: true });
+        });
+    }
+  }, [user])
 
 
 
+
+  // succesfull 
+  useEffect(() => {
+    if (loguser) {
+      toast.success('LogIn Successful')
+    }
+  }, [loguser])
+
+
+  // User sign up error
+  useEffect(() => {
+    if (error?.code) {
+      toast("Opps!! No User Found")
+    }
+  }, [error])
+
+
+  // password reset
+  const resetPassword = async () => {
+    if (email === '') {
+      setEmailError('Please Give Me A Varified Email')
+    } else {
+      await sendPasswordResetEmail(email);
+      toast('Sent Email')
+      setEmailError('')
+    }
+
+  }
 
 
 
 
 
   return (
-    <div className=''>
-      <div className="limiter">
-        <div className="container-login100" >
-          <div className="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-            <form onSubmit={handleSignin} className="login100-form validate-form flex-sb flex-w p-5">
-              <span className="login100-form-title p-b-53 py-2">
-                Sign In With
-              </span>
+    <div className="login-container">
+      <div className='mb-24 mx-5 '>
+        <div className="w-full lg:w-1/2 md:w-3/4 mx-auto bg-opacity-90 shadow-lg relative">
 
-              <Sociallogin></Sociallogin>
+          <div className=" p-5 mt-[8rem]   bg-opacity-60 backdrop-blur-lg rounded-lg ">
+            <h1 className='text-4xl font-semibold mb-5'>log In</h1>
+            <div className="h-[1px] w-full bg-black opacity-20 mb-10"></div>
+            <div className="flex flex-col text-left mb-8">
+              <label className=' text-xl ml-2 mb-2' htmlFor="email">Email</label>
+              <input onChange={emailClick} className='py-2 rounded-lg shadow-md border-0 outline-0 px-4 text-xl' type="email" name="email" id="email" required />
+            </div>
+            {
+              emailError ? <p className='text-left mb-7 text-red-600 flex'><MdOutlineCancel className='w-5 mr-2'></MdOutlineCancel> {emailError}</p> : ''
+            }
 
-              <div className="p-t-31 p-b-9 p-2">
-                <span className="txt1">
-                  Email
-                </span>
+            <div className="flex flex-col text-left mb-8">
+              <label className=' text-xl  ml-2 mb-2' htmlFor="password">Password</label>
+              <div className="relative">
+                <input onChange={passwordClick} className='w-full py-2 rounded-lg shadow-md border-0 outline-0 px-4 text-xl ' type={state ? 'text' : 'password'} name="password" id="password" required />
+
+                <div onClick={toggleBtn} className='w-6 absolute right-4 top-[25%]'>
+                  {
+                    state ? <AiFillEye ></AiFillEye> : <AiFillEyeInvisible></AiFillEyeInvisible>
+                  }
+
+                </div>
               </div>
-              <div className="wrap-input100 validate-input" data-validate="Username is required">
-                <input onBlur={(e) => setEmail(e.target.value)} className="input100" type="email" name="username" required />
-                <span className="focus-input100"></span>
-              </div>
-
-              <div className="p-t-13 p-b-9 p-2">
-                <span className="txt1 me-4">
-                  Password
-                </span>
-
-                <a href="#" onClick={async () => {
-                  await sendPasswordResetEmail(email);
-                }} className="txt2 bo1 m-l-5 ">
-                  Forgot pass enter email and click here ?
-                </a>
-              </div>
-              <div className="wrap-input100 validate-input mb-3" data-validate="Password is required">
-                <input onBlur={(e) => setPassword(e.target.value)} className="input100" type="password" name="pass" required />
-                <span className="focus-input100"></span>
-              </div>
-
-              <div className="container-login100-form-btn m-t-17">
-                <button type='submit' className="login100-form-btn">
-                  Sign In
-                </button>
-
-              </div>
-
-              <div className="w-full text-center p-t-55">
-                <span className="txt2">
-                  Not a member?
-                </span>
-
-                <Link to={'/signup'} className="txt2 bo1">Sign up Now</Link>
-              </div>
-            </form>
+            </div>
+            {
+              passError ? <p className='text-left mb-7 text-red-600 flex'><MdOutlineCancel className='w-5 mr-2'></MdOutlineCancel> {passError}</p> : ''
+            }
+            <div className="btn-container mt-10 mb-3">
+              <button onClick={handelLogin} className='w-1/3 bg-teal-500 shadow-md py-1 text-xl rounded text-white'>Log In</button>
+            </div>
+            <p className='mb-4 font-medium'>Create New Account? <span onClick={() => naviget('/signup')} className=' text-teal-500 cursor-pointer '>Register</span></p>
+            <div onClick={() => signInWithGoogle()} className=" cursor-pointer flex items-center justify-center w-11/12 md:w-7/12 mx-auto bg-slate-50 rounded-lg shadow-md mb-5" >
+              <img className='w-[45px] mr-2' src="https://pngimg.com/uploads/google/google_PNG19635.png" alt="" />
+              <h3 className=' font-semibold'>Continue With Google</h3>
+            </div>
           </div>
         </div>
       </div>
-
-
-      <div id="dropDownSelect1"></div>
     </div>
   );
 };
