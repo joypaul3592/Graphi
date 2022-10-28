55330db4916d320273eeeea37dfa24d77454d798
Mosiur <mosiurislamwebdesign@gmail.com>
2022-10-28 11:54:58 +0600

    samthiok edit

--- src/Components/Chart/BackendDatahendel.js
+++ src/Components/Chart/BackendDatahendel.js
@@ -1,6 +1,6 @@
 import { ChartDatapass, DataPassDekhi } from "./ChartDatapass"
 const PostData = (pathlocation, userIdentify, Data, setdataisLoaded, dataisLoaded, e) => {
-    fetch(`https://blooming-meadow-86067.herokuapp.com/api/v1/grap/${pathlocation}/?user=${userIdentify}`, {
+    fetch(`http://localhost:5000/api/v1/grap/${pathlocation}/?user=${userIdentify}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -20,7 +20,7 @@ const PostData = (pathlocation, userIdentify, Data, setdataisLoaded, dataisLoade
         })
 }
 const GetData = (pathlocation, userIdentify, setData, setDelete) => {
-    fetch(`https://blooming-meadow-86067.herokuapp.com/api/v1/grap/${pathlocation}/?user=${userIdentify}`, {
+    fetch(`http://localhost:5000/api/v1/grap/${pathlocation}/?user=${userIdentify}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -37,7 +37,7 @@ const GetData = (pathlocation, userIdentify, setData, setDelete) => {
 const UpdateData = (index, pathlocation, value, setback) => {
     const id = DataPassDekhi[index]._id
     if (id) {
-        fetch(`https://blooming-meadow-86067.herokuapp.com/api/v1/grap/${pathlocation}/${id}`, {
+        fetch(`http://localhost:5000/api/v1/grap/${pathlocation}/${id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
@@ -55,7 +55,7 @@ const UpdateData = (index, pathlocation, value, setback) => {
 const DeleteData = (Delete, pathlocation, counter, setCounter) => {
     const id = Delete;
     if (id) {
-        fetch(`https://blooming-meadow-86067.herokuapp.com/api/v1/grap/${pathlocation}/${id}`, {
+        fetch(`http://localhost:5000/api/v1/grap/${pathlocation}/${id}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
